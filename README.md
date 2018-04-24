b953edbee52da524e87fd0e2c78fb4a390317024
kazehaiya <763416559@qq.com>
2018-04-24 22:31:26 +0800

    添加新例子

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 # 项目目录  
 1. 对于Number的一些格式化处理
 2. CSS实现导航栏下划线
+
+# 参考来源  
+[MDN](https://developer.mozilla.org/zh-CN/search?q=toLocaleString)  
+[不可思议的CSS导航栏下划线跟随效果](https://github.com/chokcoco/iCSS/issues/33)
--- 对于Number的一些格式化处理/toLocaleString.js
+++ 对于Number的一些格式化处理/toLocaleString.js
@@ -1,23 +1,29 @@
-(function() {
+(function () {
 	// Number.prototype.toLocalString
 
 	// const num = 10000;
 	// console.log(num.toLocaleString());	// 10,000
 
-	// console.log(num.toLocaleString('zh', {style: 'decimal'}));	// 10,000
+	// console.log(num.toLocaleString('zh', { style: 'decimal'  })); // 10,000
 	// console.log(num.toLocaleString('zh', { style: 'percent' }));	// 1,000,000%
-	// console.log(num.toLocaleString('zh', { style: 'currency' }));	// 报错
+	// console.log(num.toLocaleString('zh', { style: 'currency' })); // 报错
 
 
-	const num = 123.456;
-	// 最少5位整数
-	console.log(num.toLocaleString('zh', {minimumIntegerDigits: 5}));	 // 00,123.456
-	// 最少5位整数，不带分隔符
-	console.log(num.toLocaleString('zh', {minimumIntegerDigits: 5, useGrouping: false}));	 // 00123.456
-	// 最少5位小数，不带分隔符
-	console.log(num.toLocaleString('zh', {minimumFractionDigits: 5, useGrouping: false}));	// 123.45600
-	// 最多2位小数，不带分隔符
-	console.log(num.toLocaleString('zh', {maximumFractionDigits: 2, useGrouping: false}));	// 123.46
-
-	
+	// const num = 123.456;
+	// // 最少5位整数
+	// console.log(num.toLocaleString('zh', { minimumIntegerDigits: 5 }));	// 00,123.456
+	// // 最少5位整数，不带分隔符
+	// console.log(num.toLocaleString('zh', { minimumIntegerDigits: 5, useGrouping: false }));	// 00123.456
+	// // 最少5位小数，不带分隔符
+	// console.log(num.toLocaleString('zh', { minimumFractionDigits: 5, useGrouping: false })); // 123.45600
+	// // 最多2位小数，不带分隔符
+	// console.log(num.toLocaleString('zh', { maximumFractionDigits: 2, useGrouping: false })); // 123.46
+	// // 有效数字最小为9位
+	// console.log(num.toLocaleString('zh', { minimumSignificantDigits: 9, useGrouping: false }));	// 123.456000
+	// // 有效数字最大为4位
+	// console.log(num.toLocaleString('zh', { maximumSignificantDigits: 4, useGrouping: false }));	// 123.5
+	// // 对比
+	// let strNum = num.toString();
+	// console.log(strNum.padStart(9, '0'));	// 00123.456
+	// console.log(strNum.padEnd(9, '0'));	// 123.45600
 })();
